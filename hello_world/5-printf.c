a575f55e8fb7fb2c1f757cc45ce14de89c027389
wydnickk <nnamazova374@gmail.com>
2025-11-30 09:23:36 +0000

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the line:
+ * "with proper grammar, but the outcome is a piece of art,",
+ * followed by a new line. It uses the puts function and
+ * returns 0 upon successful completion.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
