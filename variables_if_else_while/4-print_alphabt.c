732de73cae27755b30096b07190c92a058d0418e
wydnickk <nnamazova374@gmail.com>
2025-12-05 10:09:14 +0000

    .

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+			continue;
+
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
