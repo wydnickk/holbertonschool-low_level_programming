ae39094f8d71186ca9b472ba645ab126dd0425a3
wydnickk <nnamazova374@gmail.com>
2025-12-06 21:30:10 +0400

    m

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,9 +4,7 @@
  * print_sign - prints the sign of a number
  * @n: the number to check
  *
- * Return: 1 if n is greater than zero,
- *         0 if n is zero,
- *        -1 if n is less than zero
+ * Return: 1 if n > 0, 0 if n == 0, -1 if n < 0
  */
 int print_sign(int n)
 {
