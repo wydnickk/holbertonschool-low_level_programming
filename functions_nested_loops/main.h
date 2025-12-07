b5c3663048d6a98c66773639db320a6056e8d1f0
wydnickk <nnamazova374@gmail.com>
2025-12-07 02:42:35 +0000

    n

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,12 +2,13 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
+
+/* buraya funksiyalarınızın prototiplərini əlavə edin */
 int print_sign(int n);
 int _abs(int n);
+int _islower(int c);
+int _isalpha(int c);
 int print_last_digit(int n);
+void jack_bauer(void);
 
 #endif /* MAIN_H */
