f35eff11b4b70458eab3bbbcd5a58f7d334aa42e
wydnickk <nnamazova374@gmail.com>
2025-12-14 06:22:58 +0000

    Pointer/C

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
