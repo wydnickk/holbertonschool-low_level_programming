ea318bbee008a5222d6b242a476181e277a4ee5a
wydnickk <nnamazova374@gmail.com>
2025-12-28 10:03:56 +0000

    Add malloc_free create_array task and README

---
+++ malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * create_array - creates and initializes char array
+ * @size: array size
+ * @c: initial value
+ * Return: pointer to array or NULL
+ */
+char *create_array(unsigned int size, char c);
+
+#endif
